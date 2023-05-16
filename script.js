c757719d1e50ed231cdd07c71276608923211949
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-05-16 22:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 //your code here
+const table = document.querySelector("table");
+const lastRow = document.createElement("tr");
+// const prices = document.querySelectorAll("[data-ns-test=price]");
+let allRows = document.getElementsByTagName("tr");
+let sum = 0;
+for (let i = 0; i < allRows.length; i++) {
+  let tds = allRows[i].getElementsByTagName("td");
+  sum += parseInt(tds[2].textContent);
+}
 
- 
-//your code here
- const table = document.querySelector('table');
- const lastRow = document.createElement('tr');
- // const prices = document.querySelectorAll('[data-ns-test=price]');
- let sum = 0;
- // for(let i = 0; i < prices.length; i++) {
- //  sum += parseInt(prices[i].textContent);
- // }
+// for (let i = 0; i < prices.length; i++) {
+//   sum += parseInt(prices[i].textContent);
+// }
+const child = document.createElement("td");
+child.setAttribute("data-ns-test", "grandTotal");
 
-// const allRows=document.getElementsByTagName("tr");
-
-for(let i=0;i<allRows.length;i++)
-	{
-		let eachrow=allRows[i].lastChild;
-		sum+=parseInt(eachrow.textContent);
-		
-	}
-
- const child = document.createElement("td");
- child.setAttribute('data-ns-test', 'grandTotal');
- 
- 
- child.textContent = sum;
- lastRow.appendChild(child);
- table.appendChild(lastRow);
+child.textContent = sum;
+lastRow.appendChild(child);
+table.appendChild(lastRow);
