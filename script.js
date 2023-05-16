3b833c3f6337e0628d757c37d42ba90baa3127ff
Fayeemahmad398 <117560978+Fayeemahmad398@users.noreply.github.com>
2023-05-16 22:39:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,20 +4,20 @@
 //your code here
  const table = document.querySelector('table');
  const lastRow = document.createElement('tr');
- const prices = document.querySelectorAll('[data-ns-test=price]');
+ // const prices = document.querySelectorAll('[data-ns-test=price]');
  let sum = 0;
- for(let i = 0; i < prices.length; i++) {
-  sum += parseInt(prices[i].textContent);
- }
+ // for(let i = 0; i < prices.length; i++) {
+ //  sum += parseInt(prices[i].textContent);
+ // }
 
 // const allRows=document.getElementsByTagName("tr");
 
-// for(let i=0;i<allRows.length;i++)
-// 	{
-// 		let eachrow=allRows[i].lastChild;
-// 		sum+=eachrow.textContent;
+for(let i=0;i<allRows.length;i++)
+	{
+		let eachrow=allRows[i].lastChild;
+		sum+=parseInt(eachrow.textContent);
 		
-// 	}
+	}
 
  const child = document.createElement("td");
  child.setAttribute('data-ns-test', 'grandTotal');
